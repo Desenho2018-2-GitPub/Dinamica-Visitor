ac183f5100f21f86f136f7ecaf04438b47cea894
Arthur Diniz <arthurbdiniz@gmail.com>
2018-10-07 23:57:35 -0300

    Visitor example
    Signed-off-by: Arthur Diniz <arthurbdiniz@gmail.com>

---
+++ src/Visitor.java
@@ -0,0 +1,13 @@
+interface Visitor {
+	
+	// Created to automatically use the right 
+	// code based on the Object sent
+	// Method Overloading
+	
+	public double visit(Liquor liquorItem);
+	
+	public double visit(Tobacco tobaccoItem);
+	
+	public double visit(Necessity necessityItem);
+	
+}
